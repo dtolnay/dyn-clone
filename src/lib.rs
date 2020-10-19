1f7f509f366aaff378d29650da9879a50a9e4efe
David Tolnay <dtolnay@gmail.com>
2020-10-19 11:57:36 -0700

    Clean trailing whitespace from doc comment block

--- src/lib.rs
+++ src/lib.rs
@@ -11,38 +11,38 @@
 //! implementation of `DynClone`. Types that implement the standard library's
 //! [`std::clone::Clone`] trait are automatically usable by a `DynClone` trait
 //! object.
-//! 
+//!
 //! [`DynClone`]: trait.DynClone.html
 //! [`clone_box`]: fn.clone_box.html
 //! [`std::clone::Clone`]: https://doc.rust-lang.org/std/clone/trait.Clone.html
-//! 
+//!
 //! # Example
-//! 
+//!
 //! ```
 //! use dyn_clone::DynClone;
 //!
 //! trait MyTrait: DynClone {
 //!     fn recite(&self);
 //! }
-//! 
+//!
 //! impl MyTrait for String {
 //!     fn recite(&self) {
 //!         println!("{} ♫", self);
 //!     }
 //! }
-//! 
+//!
 //! fn main() {
 //!     let line = "The slithy structs did gyre and gimble the namespace";
-//! 
+//!
 //!     // Build a trait object holding a String.
 //!     // This requires String to implement MyTrait and std::clone::Clone.
 //!     let x: Box<dyn MyTrait> = Box::new(String::from(line));
-//! 
+//!
 //!     x.recite();
-//! 
+//!
 //!     // The type of x2 is a Box<dyn MyTrait> cloned from x.
 //!     let x2 = dyn_clone::clone_box(&*x);
-//! 
+//!
 //!     x2.recite();
 //! }
 //! ```
