1500eb86a675c0b5852c4ef0ae63c05541902233
David Tolnay <dtolnay@gmail.com>
2022-03-15 12:30:15 -0700

    Release 1.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/dyn_clone/1.0.4")]
+#![doc(html_root_url = "https://docs.rs/dyn_clone/1.0.5")]
 #![no_std]
 #![allow(clippy::missing_panics_doc)]
 
