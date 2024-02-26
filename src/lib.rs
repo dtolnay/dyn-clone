51bf8816be5a73e38b59fd4d9dda2bc18e9c2429
David Tolnay <dtolnay@gmail.com>
2024-02-25 22:08:30 -0800

    Release 1.0.17

--- src/lib.rs
+++ src/lib.rs
@@ -80,7 +80,7 @@
 //! // and similar for Box<dyn MyTrait + Send>, Box<dyn MyTrait + Sync>, Box<dyn MyTrait + Send + Sync>
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/dyn-clone/1.0.16")]
+#![doc(html_root_url = "https://docs.rs/dyn-clone/1.0.17")]
 #![no_std]
 #![allow(
     clippy::missing_panics_doc,
